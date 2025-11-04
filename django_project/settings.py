08f0793a4f105372b64c65a48a84deff30e28822
KyleHoffman24 <corginuggets1@gmail.com>
2025-11-04 16:37:47 +0000

    Fix formatting in settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "pages",
 ]
 
-MIDDLEWARE =    
+MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
